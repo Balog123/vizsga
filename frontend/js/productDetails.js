fa8ceca9dd5e44eaf126c3901d9bbc02c8c561d1
csernikrisztián <cserni.krisztian@BOLYAI.LOCAL>
2024-02-20 14:47:58 +0100

    dinamikus termékmegjelenítés próbálkozás

--- frontend/js/productDetails.js
+++ frontend/js/productDetails.js
@@ -1,26 +1,38 @@
-window.addEventListener('load', () => {
-    // Az URL-ből kinyerjük a termekId-t
+// productDetails.js
+
+window.addEventListener("load", () => {
+    // A query stringből kinyerjük a termék azonosítóját
     const urlParams = new URLSearchParams(window.location.search);
     const termekId = urlParams.get('termekId');
 
-    // Ellenőrizzük, hogy van-e termekId az URL-ben
+    // Ellenőrizzük, hogy van-e termék azonosító
     if (termekId) {
-        // Ha van, akkor kérjük le a termék részleteit az adatbázisból
+        // Fetcheljük a termék részleteit a szerverről
         fetch(`http://localhost:8000/termek/${termekId}`)
-            .then(response => response.json())
-            .then(termekDetails => {
-                // Itt dolgozd fel a termék részleteit és jelenítsd meg az oldalon
+            .then((response) => response.json())
+            .then((data) => {
+                // Megtaláltuk a termék részleteit
+                const termekAdatok = data.termekAdatok;
 
-                // Példa: termék nevének megjelenítése
-                const termekNevDiv = document.getElementById('termek-nev');
-                termekNevDiv.textContent = termekDetails.termek_nev;
-
-                // Példa: termék képének megjelenítése
-                const termekKepDiv = document.getElementById('termek-kep');
-                termekKepDiv.innerHTML = `<img src="${termekDetails.kep_url1}" alt="Termék kép" style="width: 100%;">`;
-
-                // További részletek megjelenítése...
+                // Megjelenítjük a termék részleteit a HTML-ben
+                displayTermekDetails(termekAdatok);
             })
-            .catch(error => console.error('Hiba a termék részleteinek lekérése során', error));
+            .catch((error) =>
+                console.error("Hiba a termék információ lekérése során", error)
+            );
     }
 });
+
+function displayTermekDetails(termekAdatok) {
+    // Kép megjelenítése
+    const termekKepContainer = document.getElementById("termek-kep");
+    termekKepContainer.innerHTML = `<img src="${termekAdatok.kep_url}" alt="Termék kép">`;
+
+    // Termék nevének megjelenítése
+    const termekNevElem = document.getElementById("termek-nev");
+    termekNevElem.textContent = termekAdatok.termek_nev;
+
+    // Ár megjelenítése
+    const termekArElem = document.querySelector(".price");
+    termekArElem.textContent = `${termekAdatok.termek_ar} Ft`;
+}
