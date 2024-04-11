b98c4553591d648bf6b241e1836df7fc8b476c37
test <test@bolyai.local>
2024-04-11 14:49:43 +0200

    teszt + javítás

--- frontend/cypress/e2e/index.cy.js
+++ frontend/cypress/e2e/index.cy.js
@@ -22,3 +22,41 @@ describe('Navigációs sáv változásának ellenőrzése', () => {
     cy.get('.header').should('have.class', 'scrolled');
   });
 });
+
+describe('Navigációs sáv URL linek ellenőrzése', () => {
+  it('Kattintás a "Kezdőlap" hivatkozásra és URL ellenőrzése', () => {
+    cy.visit('http://localhost:8000');
+    cy.get('.nav-link').scrollTo
+  })
+});
+
+describe('Ikonok betöltődésének ellenőrzése', () => {
+  it('Profil ikon URL ell', () => {
+    cy.visit('http://localhost:8000');
+    cy.get('#userIcon').click().then(() => {
+      cy.url().should('eq', 'http://localhost:8000/regisztracio');
+      cy.get('h1').should('contain', 'Regisztráció');
+    });
+  });
+
+  it('Keresés ikon ellenőrzése', () => {
+    cy.visit('http://localhost:8000');
+    cy.get('#searchIcon').click().then(() => {
+      cy.get('#searchBar.search-bar-container').should('exist');
+    });
+  });
+
+  it('Kosár ikon ellenőrzése', () => {
+    cy.visit('http://localhost:8000');
+    cy.get('#kosaricon').click().then(() => {
+      cy.url().should('eq', 'http://localhost:8000/kosar');
+
+      cy.get('#cart-table').should('exist');
+      cy.get('#cart-table thead th').eq(0).should('contain', 'Termék neve');
+      cy.get('#cart-table thead th').eq(1).should('contain', 'Kép');
+      cy.get('#cart-table thead th').eq(2).should('contain', 'Ár');
+      cy.get('#cart-table thead th').eq(3).should('contain', 'Mennyiség');
+      cy.get('#cart-table thead th').eq(4).should('contain', 'Módosítás');
+    });
+  });
+});
